bbe3a80e9748b2f0f945d45adcaeb26478d91fbb
ahkamulbd <ahkamulbd@gmail.com>
2022-12-26 10:53:20 +0600

    Module 33 recapping initiated and 33-1 done

---
+++ js/kanye.js
@@ -0,0 +1,13 @@
+//console.log('Here is Kanye Uncle');
+
+const loadQuote = () => {
+    fetch('https://api.kanye.rest')
+        .then(res => res.json())
+        .then(data => displayQuote(data))
+}
+
+const displayQuote = quote => {
+    const blockQuote = document.getElementById('quote');
+    console.log(quote);
+    blockQuote.innerText = quote.quote;
+}
